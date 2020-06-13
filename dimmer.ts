5052ed16f60f1f82d3af9822c5936c73af3deb65
Alexander Groß <agross@therightstuff.de>
2020-06-14 00:16:01 +0200

    Favor composition over inheritance

--- dimmer.ts
+++ dimmer.ts
@@ -3,8 +3,8 @@ import { filter, debounceTime, tap } from 'rxjs/operators';
 
 const BRIGHTNESS_CHANGE = 5;
 
-abstract class OnOffStrategy {
-  public abstract setUp(config: Config): void;
+abstract class OnOffStrategy implements IFeature {
+  public abstract setUp(device: string): void;
 }
 
 interface CycleConfig {
@@ -20,12 +20,14 @@ class Cycle extends OnOffStrategy {
     this.config = config;
   }
 
-  public setUp(config: Config): void {
-    on({ id: `${config.device}.state`, ack: true, val: false }, off => {
+  public setUp(device: string): void {
+    on({ id: `${device}.state`, ack: true, val: false }, off => {
+      log(`${device}: Turning off`);
+
       setState(this.config.off, false);
     });
 
-    on({ id: `${config.device}.state`, ack: true, val: true }, on => {
+    on({ id: `${device}.state`, ack: true, val: true }, on => {
       const currentIndex = this.config.on
         .map(obj => {
           return { object: obj, state: getState(obj) };
@@ -39,7 +41,7 @@ class Cycle extends OnOffStrategy {
         }, -1);
 
       const activate = this.config.on[currentIndex + 1] || this.config.on[0];
-      log(`Cycle next: ${activate}`);
+      log(`${device}: Cycle next: ${activate}`);
 
       setState(activate, true);
     });
@@ -54,70 +56,90 @@ class Toggle extends OnOffStrategy {
     this.objects = objects;
   }
 
-  public setUp(config: Config): void {
-    on({ id: `${config.device}.state`, ack: true }, event => {
+  public setUp(device: string): void {
+    on({ id: `${device}.state`, ack: true }, event => {
       this.objects.forEach(obj => setState(obj, event.state.val));
     });
   }
 }
 
-interface Config {
+interface RemoteConfig {
   device: string;
-  change: number;
-  lights: string[] | DimmableLights;
-  on_off?: OnOffStrategy;
 }
 
-abstract class Driver {
-  protected readonly config: Config;
+abstract class Remote {
+  private readonly features: IFeature[] = [];
+  private readonly config: RemoteConfig;
 
-  constructor(args: Config) {
-    this.config = args;
+  constructor(config: RemoteConfig) {
+    this.config = config;
   }
 
-  protected abstract darker(cb: (event: iobJS.ChangedStateObject) => any): void;
-  protected abstract brighter(
-    cb: (event: iobJS.ChangedStateObject) => any,
-  ): void;
-  protected abstract stop(cb: (event: iobJS.ChangedStateObject) => any): void;
-
   public setUp(): void {
-    const darker = () =>
-      this.dim(
-        (ref, brightness) => brightness < (ref || Infinity),
-        brightness => brightness - this.config.change,
-      );
-    const brighter = () =>
-      this.dim(
-        (ref, brightness) => brightness > (ref || -Infinity),
-        brightness => brightness + this.config.change,
-      );
+    this.features.forEach(f => f.setUp(this.config.device));
+  }
 
+  protected addFeature(...feature: IFeature[]): void {
+    feature.forEach(feature => this.features.push(feature));
+  }
+}
+
+interface IFeature {
+  setUp(device: string): void;
+}
+
+interface DimmerConfig {
+  brightnessChange: number;
+  lights: string[] | DimmableLights;
+  brighter: (
+    device: string,
+    cb: (event: iobJS.ChangedStateObject) => any,
+  ) => void;
+  darker: (
+    device: string,
+    cb: (event: iobJS.ChangedStateObject) => any,
+  ) => void;
+  stop: (device: string, cb: (event: iobJS.ChangedStateObject) => any) => void;
+}
+
+class Dimmer implements IFeature {
+  readonly config: DimmerConfig;
+
+  constructor(config: DimmerConfig) {
+    this.config = config;
+  }
+
+  setUp(device: string): void {
     let interval = undefined;
 
-    this.darker(_event => {
+    this.config.darker(device, _event => {
       if (!interval) {
-        log(`Starting ${this.config.device} darker dimmer`);
+        log(`Starting ${device} darker dimmer`);
+
         this.config.lights.forEach(light => {
           setState(`${light}.transition_time`, 0.2);
         });
-        interval = setInterval(darker, 200);
+
+        interval = setInterval(() => this.makeDarker(), 200);
       }
     });
 
-    this.brighter(_event => {
+    this.config.brighter(device, _event => {
       if (!interval) {
-        log(`Starting ${this.config.device} brighter dimmer`);
+        log(`Starting ${device} brighter dimmer`);
+
         this.config.lights.forEach(light => {
           setState(`${light}.transition_time`, 0.2);
         });
-        interval = setInterval(brighter, 200);
+
+        interval = setInterval(() => this.makeBrighter(), 200);
       }
     });
 
-    this.stop(_event => {
+    this.config.stop(device, _event => {
       if (interval) {
-        log(`Stopping ${this.config.device} dimmer`);
+        log(`Stopping ${device} dimmer`);
+
         clearInterval(interval);
         interval = undefined;
 
@@ -126,13 +148,23 @@ abstract class Driver {
         });
       }
     });
-
-    if (this.config.on_off) {
-      this.config.on_off.setUp(this.config);
-    }
   }
 
-  private dim(
+  private makeDarker() {
+    this.changeBrightness(
+      (ref, brightness) => brightness < (ref || Infinity),
+      brightness => brightness - this.config.brightnessChange,
+    );
+  }
+
+  private makeBrighter() {
+    this.changeBrightness(
+      (ref, brightness) => brightness > (ref || -Infinity),
+      brightness => brightness + this.config.brightnessChange,
+    );
+  }
+
+  private changeBrightness(
     selector: (reference: number, brightness: number) => boolean,
     newBrightness: (brightness: number) => number,
   ) {
@@ -152,7 +184,6 @@ abstract class Driver {
 
     log(
       `Picked ${minBrightness.source} "${minBrightness.state.val}" as reference`,
-      'info',
     );
 
     let brightness = newBrightness(minBrightness.state.val);
@@ -168,38 +199,81 @@ abstract class Driver {
   }
 }
 
-class Tradfi extends Driver {
-  protected darker(cb: (event: iobJS.ChangedStateObject) => any) {
-    on(
-      { id: `${this.config.device}.down_button`, val: true, ack: true },
-      event => cb(event),
+interface DimConfig {
+  brightnessChange: number;
+  lights: string[] | DimmableLights;
+}
+
+interface CycleConfig {
+  off: string;
+  on: string[];
+}
+
+interface OnOffDimRemoteConfig extends RemoteConfig {
+  dim: DimConfig;
+  on_off: OnOffStrategy;
+}
+
+class Tradfi extends Remote {
+  constructor(config: OnOffDimRemoteConfig) {
+    super(config);
+
+    this.addFeature(
+      config.on_off,
+      new Dimmer({
+        lights: config.dim.lights,
+        brightnessChange: config.dim.brightnessChange,
+        brighter: (device, cb) => this.brighter(device, cb),
+        darker: (device, cb) => this.darker(device, cb),
+        stop: (device, cb) => this.stop(device, cb),
+      }),
     );
   }
 
-  protected brighter(cb: (event: iobJS.ChangedStateObject) => any) {
-    on({ id: `${this.config.device}.up_button`, val: true, ack: true }, event =>
+  private darker(device: string, cb: (event: iobJS.ChangedStateObject) => any) {
+    on({ id: `${device}.down_button`, val: true, ack: true }, event =>
       cb(event),
     );
   }
 
-  protected stop(cb: (event: iobJS.ChangedStateObject) => any) {
-    on(
-      { id: `${this.config.device}.down_button`, val: false, ack: true },
-      event => cb(event),
+  private brighter(
+    device: string,
+    cb: (event: iobJS.ChangedStateObject) => any,
+  ) {
+    on({ id: `${device}.up_button`, val: true, ack: true }, event => cb(event));
+  }
+
+  private stop(device: string, cb: (event: iobJS.ChangedStateObject) => any) {
+    on({ id: `${device}.down_button`, val: false, ack: true }, event =>
+      cb(event),
     );
-    on(
-      { id: `${this.config.device}.up_button`, val: false, ack: true },
-      event => cb(event),
+    on({ id: `${device}.up_button`, val: false, ack: true }, event =>
+      cb(event),
     );
   }
 }
 
-class Philips extends Driver {
-  private _up_hold;
-  private _down_hold;
+class Philips extends Remote {
+  private _up_hold: Observable<iobJS.ChangedStateObject>;
+  private _down_hold: Observable<iobJS.ChangedStateObject>;
 
-  protected darker(cb: (event: iobJS.ChangedStateObject) => any) {
-    this.down_hold()
+  constructor(config: OnOffDimRemoteConfig) {
+    super(config);
+
+    this.addFeature(
+      config.on_off,
+      new Dimmer({
+        lights: config.dim.lights,
+        brightnessChange: config.dim.brightnessChange,
+        brighter: (device, cb) => this.brighter(device, cb),
+        darker: (device, cb) => this.darker(device, cb),
+        stop: (device, cb) => this.stop(device, cb),
+      }),
+    );
+  }
+
+  private darker(device: string, cb: (event: iobJS.ChangedStateObject) => any) {
+    this.down_hold(device)
       .pipe(
         filter((event: iobJS.ChangedStateObject) => event.state.val === true),
         tap((event: iobJS.ChangedStateObject) => cb(event)),
@@ -207,8 +281,11 @@ class Philips extends Driver {
       .subscribe();
   }
 
-  protected brighter(cb: (event: iobJS.ChangedStateObject) => any) {
-    this.up_hold()
+  private brighter(
+    device: string,
+    cb: (event: iobJS.ChangedStateObject) => any,
+  ) {
+    this.up_hold(device)
       .pipe(
         filter((event: iobJS.ChangedStateObject) => event.state.val === true),
         tap((event: iobJS.ChangedStateObject) => cb(event)),
@@ -216,8 +293,8 @@ class Philips extends Driver {
       .subscribe();
   }
 
-  protected stop(cb: (event: iobJS.ChangedStateObject) => any) {
-    merge(this.down_hold(), this.up_hold())
+  private stop(device: string, cb: (event: iobJS.ChangedStateObject) => any) {
+    merge(this.down_hold(device), this.up_hold(device))
       .pipe(
         filter((event: iobJS.ChangedStateObject) => event.state.val === false),
         debounceTime(900),
@@ -226,13 +303,13 @@ class Philips extends Driver {
       .subscribe();
   }
 
-  private down_hold() {
+  private down_hold(device: string) {
     if (this._down_hold) {
       return this._down_hold;
     }
 
-    this._down_hold = Observable.create(observer => {
-      on({ id: `${this.config.device}.down_hold`, ack: true }, event => {
+    this._down_hold = new Observable(observer => {
+      on({ id: `${device}.down_hold`, ack: true }, event => {
         observer.next(event);
       });
     });
@@ -240,13 +317,13 @@ class Philips extends Driver {
     return this._down_hold;
   }
 
-  private up_hold() {
+  private up_hold(device: string) {
     if (this._up_hold) {
       return this._up_hold;
     }
 
-    this._up_hold = Observable.create(observer => {
-      on({ id: `${this.config.device}.up_hold`, ack: true }, event => {
+    this._up_hold = new Observable(observer => {
+      on({ id: `${device}.up_hold`, ack: true }, event => {
         observer.next(event);
       });
     });
@@ -320,10 +397,11 @@ class DimmableLights {
 
 const remotes = [
   new Tradfi({
-    // Kitchen TRADFRI on/off switch
-    device: 'zigbee.0.588e81fffe2bacf4',
-    change: BRIGHTNESS_CHANGE,
-    lights: new DimmableLights({ rooms: 'Kitchen', functions: 'funcLight' }),
+    device: 'zigbee.0.588e81fffe2bacf4', // Kitchen TRADFRI on/off switch
+    dim: {
+      brightnessChange: BRIGHTNESS_CHANGE,
+      lights: new DimmableLights({ rooms: 'Kitchen', functions: 'funcLight' }),
+    },
     on_off: new Cycle({
       off: 'scene.0.Kitchen_Lights',
       on: [
@@ -338,8 +416,10 @@ const remotes = [
   new Tradfi({
     // Bedroom TRADFRI on/off switch
     device: 'zigbee.0.588e81fffe17a8ca',
-    change: BRIGHTNESS_CHANGE,
-    lights: new DimmableLights({ rooms: 'Bedroom', functions: 'funcLight' }),
+    dim: {
+      brightnessChange: BRIGHTNESS_CHANGE,
+      lights: new DimmableLights({ rooms: 'Bedroom', functions: 'funcLight' }),
+    },
     on_off: new Toggle(
       'zigbee.0.0017880108376b6c.state', // Bedroom Philips Hue LCA001
     ),
@@ -347,11 +427,13 @@ const remotes = [
   new Philips({
     // Hue Remote RWL021
     device: 'zigbee.0.001788010872fbc4',
-    change: BRIGHTNESS_CHANGE,
-    lights: new DimmableLights({
-      rooms: 'Living Room',
-      functions: 'funcLight',
-    }),
+    dim: {
+      brightnessChange: BRIGHTNESS_CHANGE,
+      lights: new DimmableLights({
+        rooms: 'Living Room',
+        functions: 'funcLight',
+      }),
+    },
     on_off: new Cycle({
       off: 'scene.0.Living_Room_Lights',
       on: [
