ae0aeaf1891c3c317fba176604e236d8a84538b1
Alexander Groß <agross@therightstuff.de>
2020-06-15 18:21:09 +0200

    Simplify check if any light is on

--- entrance-door-lights.ts
+++ entrance-door-lights.ts
@@ -9,39 +9,13 @@ function atNight() {
   );
 }
 
-function getStateCandidates(...candidates: string[]): string {
-  return candidates.find(candidate => existsState(candidate));
-}
-
-function anyLightsOn() {
-  const functions = getEnums('functions') as any[];
-  const lights = functions
-    .filter(fun => fun.id === 'enum.functions.funcLight')
-    .map(lights => lights.members as string[])
-    .reduce((acc, el) => (acc.push(...el), acc), []);
-
-  return lights.some(light => {
-    const match = getStateCandidates(light, light + '.state');
-    if (match === undefined) {
-      log(`No state for ${light}`);
-      return false;
-    }
-
-    const state = getState(match);
-
-    log(`Checking light ${match}: ${state.val}`);
-
-    return state.val ? state.val === true : false;
-  });
-}
-
 on({ id: sensor, val: true, ack: true }, event => {
   if (!atNight()) {
     log('Daytime, skipping');
     return;
   }
 
-  if (anyLightsOn()) {
+  if (getState('scene.0.All_Lights').val !== false) {
     log('Some lights are on, skipping');
     return;
   }
