a8aa24f706f79b30083b564f441a8c2de080c792
Alexander Groß <agross@therightstuff.de>
2020-09-05 08:31:05 +0200

    Schedule heating period for HomeMatic

---
+++ heating-period.ts
@@ -0,0 +1,23 @@
+const heatingPeriod = 'hm-rega.0.2322';
+
+// May 1st.
+schedule('0 0 1 5 *', () => {
+  setState(heatingPeriod, false);
+
+  sendTo('pushbullet', {
+    message: 'Heating period ended',
+    title: 'ioBroker',
+    type: 'note',
+  });
+});
+
+// October 1st.
+schedule('0 0 1 10 *', () => {
+  setState(heatingPeriod, false);
+
+  sendTo('pushbullet', {
+    message: 'Heating period started',
+    title: 'ioBroker',
+    type: 'note',
+  });
+});
