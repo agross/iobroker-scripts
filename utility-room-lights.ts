222b1a3c7d8976bebff559f40bc622c9b90f6499
Alexander Groß <agross@therightstuff.de>
2020-09-06 13:06:01 +0200

    Add utility room lights automation

---
+++ utility-room-lights.ts
@@ -0,0 +1,17 @@
+const sensor = 'zigbee.1.00158d00045c1216.opened';
+const scene = 'scene.0.Utility Room.Entered';
+const timeoutMinutes = 2;
+const timeout = timeoutMinutes * 60 * 1000;
+
+on({ id: sensor, oldVal: false, val: true, ack: true }, event => {
+  log(JSON.stringify(event));
+
+  setState(scene, true);
+  setStateDelayed(scene, false, timeout, true);
+});
+
+on({ id: sensor, val: false, ack: true }, event => {
+  log(JSON.stringify(event));
+
+  setState(scene, false);
+});
