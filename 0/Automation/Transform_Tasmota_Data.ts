2635b667f40a6af5daf94f68cb0be13ddae7d961
Alexander Groß <agross@therightstuff.de>
2020-09-12 16:57:35 +0200

    Transform Tasmota data using aliases

--- 0/Automation/Transform_Tasmota_Data.ts
+++ 0/Automation/Transform_Tasmota_Data.ts
@@ -1,46 +1,137 @@
-on(
-  {
-    id: /^mqtt\..*\.tele\.gosund-sp111-\d+\.SENSOR$/,
-    change: 'ne',
-    ack: true,
-  },
-  event => {
-    const json = JSON.parse(event.newState.val);
-    const power = json.ENERGY.Power as number;
-
-    log(`${event.id}: ${power} W`, 'debug');
-
-    const id = `transformed-${event.id}.Power`;
-    if (getState(id).notExist) {
-      createState(id, power, true, { unit: 'W' });
-    } else {
-      setState(id, power, true);
-    }
-  },
-);
-
-function toBool(val: string): boolean {
-  if (val === 'ON') {
-    return true;
-  }
-
-  return false;
+function alias(id: string): string {
+  return (
+    'alias.0.' + id.replace(/\.[^.]*$/, '').replace(/\.(cmnd|tele|stat)\./, '.')
+  );
 }
 
-on(
-  { id: /^mqtt\..*\.tele\.gosund-sp111-\d+\.STATE$/, change: 'ne', ack: true },
-  event => {
-    const json = JSON.parse(event.newState.val);
-    const power = json.POWER as string;
-    const powerAsBool = toBool(power);
+function getEnumIds(id: string, kind: string): string[] {
+  return (getObject(id, kind) as any).enumIds;
+}
 
-    log(`${event.id}: Power ${power} -> ${powerAsBool}`, 'debug');
+function stateIdToPurpose(stateId: string) {
+  switch (stateId.match(/\.gosund-sp111-(\d+)\./)[1]) {
+    case '1':
+      return 'Living Room Media';
 
-    const id = 'transformed-' + event.id + '.State';
-    if (getState(id).notExist) {
-      createState(id, powerAsBool, true, { type: 'boolean' });
-    } else {
-      setState(id, powerAsBool, true);
+    case '2':
+      return 'Kitchen Down Light';
+
+    case '3':
+      return 'Bathroom Washing Machine';
+
+    case '4':
+      return 'Office Desk';
+
+    default:
+      throw new Error(`No mapping from ${stateId} to purpose`);
+  }
+}
+
+const enumsToDeviceAliases: { [enumId: string]: string[] } = {};
+
+$('state[id=*.cmnd.gosund-sp111-*.POWER]').each(stateId => {
+  const lovelaceEntityType = getEnumIds(stateId, 'functions').includes(
+    'enum.functions.funcLight',
+  )
+    ? 'light'
+    : 'switch';
+
+  const aliases: { [state: string]: any } = {
+    power: {
+      alias: {
+        id: stateId.replace('.cmnd.', '.tele.').replace(/\.POWER$/, '.SENSOR'),
+        read: 'JSON.parse(val).ENERGY.Power',
+        // No write function makes this read-only.
+      },
+      role: 'value',
+      type: 'number',
+      unit: 'W',
+      read: true,
+      write: false,
+      name: `${stateIdToPurpose(stateId)} power usage`,
+    },
+    state: {
+      alias: {
+        id: { read: stateId.replace('.cmnd.', '.stat.'), write: stateId },
+        read: 'val === "ON"',
+        write: 'val === true ? "ON" : "OFF"',
+      },
+      role: 'switch',
+      type: 'boolean',
+      read: true,
+      write: true,
+      name: `${stateIdToPurpose(stateId)} power state`,
+      custom: {
+        'lovelace.0': {
+          enabled: true,
+          entity: lovelaceEntityType,
+          name: `${stateIdToPurpose(stateId).replace(/\s/g, '_')}_Power`,
+        },
+      },
+    },
+  };
+
+  const deviceAlias = alias(stateId);
+
+  const skeleton: iobJS.StateObject = {
+    type: 'state',
+    native: {},
+    common: {} as iobJS.StateCommon,
+  };
+
+  const channel: iobJS.Object = {
+    type: 'channel',
+    native: {},
+    common: { name: stateIdToPurpose(stateId), role: 'device' },
+  };
+
+  setObject(deviceAlias, channel);
+
+  getEnumIds(stateId, 'rooms')
+    .concat(getEnumIds(stateId, 'functions'))
+    .reduce((acc, enumId) => {
+      if (!acc.hasOwnProperty(enumId)) {
+        acc[enumId] = [];
+      }
+
+      acc[enumId].push(deviceAlias);
+      return acc;
+    }, enumsToDeviceAliases);
+
+  Object.entries(aliases).forEach(([state, common]) => {
+    let dup: iobJS.StateObject = Object.assign({}, skeleton);
+    Object.assign(dup, { common: common });
+
+    setObject(`${deviceAlias}.${state}`, dup, err => {
+      if (err)
+        log(
+          `${err}: ${deviceAlias}.${state}: ${JSON.stringify(
+            dup,
+            undefined,
+            2,
+          )}`,
+          'error',
+        );
+    });
+  });
+});
+
+for (let [enumId, deviceAliases] of Object.entries(enumsToDeviceAliases)) {
+  const _enum = getObject(enumId);
+  const common = (_enum.common as unknown) as { members: string[] };
+
+  const members = Array.from(common.members);
+  deviceAliases.forEach(deviceAlias => {
+    if (!members.includes(deviceAlias)) {
+      log(`Appending ${deviceAlias} to ${enumId}`);
+      members.push(deviceAlias);
     }
-  },
-);
+  });
+
+  if (members.length != common.members.length) {
+    log(`Saving ${enumId}`);
+    extendObject(enumId, { common: { members: members } });
+  }
+}
+
+stopScript(undefined);
