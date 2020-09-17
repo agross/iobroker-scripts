a09afe6893a28cacb3276395206f9722175e39ec
Alexander Groß <agross@therightstuff.de>
2020-09-17 16:14:03 +0200

    Change bathroom lights scene selection

--- 0/Lights/Bathroom_Lights.ts
+++ 0/Lights/Bathroom_Lights.ts
@@ -18,23 +18,24 @@ on(
       return;
     }
 
-    // Disabled by brightness?
-    if (getState('hm-rpc.1.000C1A49A87471.1.ILLUMINATION').val > 30) {
+    // Bright enough?
+    const illumination = getState('hm-rpc.1.000C1A49A87471.1.ILLUMINATION').val;
+    if (illumination > 20) {
       return;
     }
 
-    let scene = 'scene.0.Bathroom.Lights_Low';
+    let scene = 'scene.0.Bathroom.Lights_Default';
 
     if (compareTime('1:00', '6:00', 'between')) {
       scene = 'scene.0.Bathroom.Lights_Ultra_Low';
+
+      // If any light is on, use a brighter scene.
+      if (getState('scene.0.Lights.All_Lights_Off').val !== true) {
+        scene = 'scene.0.Bathroom.Lights_Low';
+      }
     }
 
-    // If any light is on, use a brighter scene.
-    if (getState('scene.0.Lights.All_Lights_Off').val !== true) {
-      scene = 'scene.0.Bathroom.Lights_Default';
-    }
-
-    log(`Bathroom occupied, turning on ${scene}`);
+    log(`Bathroom occupied with ${illumination} lm, turning on ${scene}`);
     setState(scene, true);
   },
 );
