cfa3c5237938f6cb56451ee6377a2f0fb4d3ef8c
Alexander Groß <agross@therightstuff.de>
2020-10-30 20:17:42 +0100

    Washing machine detection draft

---
+++ 0/Automation/Washing_Machine.ts
@@ -0,0 +1,53 @@
+import { Observable } from 'rxjs';
+import {
+  bufferCount,
+  filter,
+  first,
+  map,
+  share,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
+
+const workingThreshold = 5;
+const finishedThreshold = 20;
+const device = 'alias.0.mqtt.0.home.bathroom.power.gosund-sp111-3.power';
+
+function notify(message: string): void {
+  sendTo('pushbullet', {
+    message: message,
+    title: 'ioBroker',
+    type: 'note',
+  });
+}
+
+const powerUsage = new Observable<number>(observer => {
+  on({ id: device, ack: true }, event => {
+    observer.next(event.state.val as number);
+  });
+}).pipe(share());
+
+const running = powerUsage.pipe(
+  tap(watts => log(`Usage ${JSON.stringify(watts)}`, 'debug')),
+  filter(watts => watts >= workingThreshold),
+);
+
+const notRunning = powerUsage.pipe(
+  // Last 6 values, 10 s intervals, e.g. [3, 69, 4, 77, 3, 3, 2, 2, 2, 2]
+  bufferCount(6),
+  tap(watts => log(`Buffer ${watts}`, 'debug')),
+  map(watts => watts.reduce((acc, x) => acc + x, 0)),
+  filter(watts => watts < finishedThreshold),
+);
+
+const done = running
+  .pipe(
+    // Maybe exhaustMap is better?
+    switchMap(_ => notRunning.pipe(first())),
+    tap(_ => notify(`Washing machine has finished`)),
+  )
+  .subscribe();
+
+onStop(() => {
+  done.unsubscribe();
+});
