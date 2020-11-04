bbbf2d89c52da6a6cd4afe745e0f27f410cce6df
Alexander Groß <agross@therightstuff.de>
2020-11-04 11:39:11 +0100

    Use ObjectCreator for Tasmota data transformation aliases

--- 0/Automation/Oneshot/Transform_Tasmota_Data.ts
+++ 0/Automation/Oneshot/Transform_Tasmota_Data.ts
@@ -1,137 +1,93 @@
-function alias(id: string): string {
-  return (
-    'alias.0.' + id.replace(/\.[^.]*$/, '').replace(/\.(cmnd|tele|stat)\./, '.')
-  );
-}
-
-function getEnumIds(id: string, kind: string): string[] {
-  return (getObject(id, kind) as any).enumIds;
-}
-
-function stateIdToPurpose(stateId: string) {
-  switch (stateId.match(/\.gosund-sp111-(\d+)\./)[1]) {
-    case '1':
-      return 'Living Room Media';
-
-    case '2':
-      return 'Kitchen Down Light';
-
-    case '3':
-      return 'Bathroom Washing Machine';
-
-    case '4':
-      return 'Office Desk';
-
-    default:
-      throw new Error(`No mapping from ${stateId} to purpose`);
+function getObjectDefinition(): ObjectDefinitionRoot {
+  function deviceId(id: string): string {
+    return id.replace(/\.[^.]*$/, '').replace(/\.(cmnd|tele|stat)\./, '.');
   }
-}
 
-const enumsToDeviceAliases: { [enumId: string]: string[] } = {};
+  function stateIdToPurpose(stateId: string) {
+    switch (stateId.match(/\.gosund-sp111-(\d+)\./)[1]) {
+      case '1':
+        return 'Living Room Media';
 
-$('state[id=*.cmnd.gosund-sp111-*.POWER]').each(stateId => {
-  const lovelaceEntityType = getEnumIds(stateId, 'functions').includes(
-    'enum.functions.funcLight',
-  )
-    ? 'light'
-    : 'switch';
+      case '2':
+        return 'Kitchen Down Light';
 
-  const aliases: { [state: string]: any } = {
-    power: {
-      alias: {
-        id: stateId.replace('.cmnd.', '.tele.').replace(/\.POWER$/, '.SENSOR'),
-        read: 'JSON.parse(val).ENERGY.Power',
-        // No write function makes this read-only.
+      case '3':
+        return 'Bathroom Washing Machine';
+
+      case '4':
+        return 'Office Desk';
+
+      default:
+        throw new Error(`No mapping from ${stateId} to purpose`);
+    }
+  }
+
+  const tasmotas: ObjectDefinitionRoot = {};
+
+  $('state[id=*.cmnd.gosund-sp111-*.POWER]').each(stateId => {
+    const device = deviceId(stateId);
+
+    const lovelaceEntityType = ObjectCreator.getEnumIds(
+      stateId,
+      'functions',
+    ).includes('enum.functions.funcLight')
+      ? 'light'
+      : 'switch';
+
+    const deviceStates: {
+      [id: string]: iobJS.StateCommon & iobJS.AliasCommon & iobJS.CustomCommon;
+    } = {
+      power: {
+        alias: {
+          id: stateId
+            .replace('.cmnd.', '.tele.')
+            .replace(/\.POWER$/, '.SENSOR'),
+          read: 'JSON.parse(val).ENERGY.Power',
+          // No write function makes this read-only.
+        },
+        role: 'value',
+        type: 'number',
+        unit: 'W',
+        read: true,
+        write: false,
+        name: `${stateIdToPurpose(stateId)} power usage`,
       },
-      role: 'value',
-      type: 'number',
-      unit: 'W',
-      read: true,
-      write: false,
-      name: `${stateIdToPurpose(stateId)} power usage`,
-    },
-    state: {
-      alias: {
-        id: { read: stateId.replace('.cmnd.', '.stat.'), write: stateId },
-        read: 'val === "ON"',
-        write: 'val === true ? "ON" : "OFF"',
-      },
-      role: 'switch',
-      type: 'boolean',
-      read: true,
-      write: true,
-      name: `${stateIdToPurpose(stateId)} power state`,
-      custom: {
-        'lovelace.0': {
-          enabled: true,
-          entity: lovelaceEntityType,
-          name: Lovelace.id(`${stateIdToPurpose(stateId)} Power`),
+      state: {
+        alias: {
+          id: { read: stateId.replace('.cmnd.', '.stat.'), write: stateId },
+          read: 'val === "ON"',
+          write: 'val === true ? "ON" : "OFF"',
+        },
+        role: 'switch',
+        type: 'boolean',
+        read: true,
+        write: true,
+        name: `${stateIdToPurpose(stateId)} power state`,
+        custom: {
+          'lovelace.0': {
+            enabled: true,
+            entity: lovelaceEntityType,
+            name: Lovelace.id(`${stateIdToPurpose(stateId)} Power`),
+          },
         },
       },
-    },
-  };
+    };
 
-  const deviceAlias = alias(stateId);
-
-  const skeleton: iobJS.StateObject = {
-    type: 'state',
-    native: {},
-    common: {} as iobJS.StateCommon,
-  };
-
-  const channel: iobJS.Object = {
-    type: 'channel',
-    native: {},
-    common: { name: stateIdToPurpose(stateId), role: 'device' },
-  };
-
-  setObject(deviceAlias, channel);
-
-  getEnumIds(stateId, 'rooms')
-    .concat(getEnumIds(stateId, 'functions'))
-    .reduce((acc, enumId) => {
-      if (!acc.hasOwnProperty(enumId)) {
-        acc[enumId] = [];
-      }
-
-      acc[enumId].push(deviceAlias);
-      return acc;
-    }, enumsToDeviceAliases);
-
-  Object.entries(aliases).forEach(([state, common]) => {
-    let dup: iobJS.StateObject = Object.assign({}, skeleton);
-    Object.assign(dup, { common: common });
-
-    setObject(`${deviceAlias}.${state}`, dup, err => {
-      if (err)
-        log(
-          `${err}: ${deviceAlias}.${state}: ${JSON.stringify(
-            dup,
-            undefined,
-            2,
-          )}`,
-          'error',
-        );
-    });
-  });
-});
-
-for (let [enumId, deviceAliases] of Object.entries(enumsToDeviceAliases)) {
-  const _enum = getObject(enumId);
-  const common = (_enum.common as unknown) as { members: string[] };
-
-  const members = Array.from(common.members);
-  deviceAliases.forEach(deviceAlias => {
-    if (!members.includes(deviceAlias)) {
-      log(`Appending ${deviceAlias} to ${enumId}`);
-      members.push(deviceAlias);
-    }
+    tasmotas[device] = {
+      type: 'device',
+      native: {},
+      common: { name: stateIdToPurpose(stateId), role: 'device' },
+      enumIds: ObjectCreator.getEnumIds(stateId, 'rooms', 'functions'),
+      nested: Object.entries(deviceStates).reduce((acc, [id, common]) => {
+        acc[id] = { type: 'state', native: {}, common: common };
+        return acc;
+      }, {} as ObjectDefinitionRoot),
+    };
   });
 
-  if (members.length != common.members.length) {
-    log(`Saving ${enumId}`);
-    extendObject(enumId, { common: { members: members } });
-  }
+  return tasmotas;
 }
+
+await ObjectCreator.create(getObjectDefinition(), 'alias.0');
 
 stopScript(undefined);
