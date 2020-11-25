6b5c3da6f27fe4a21d733cc80388aff39062f724
Alexander Groß <agross@therightstuff.de>
2020-11-25 19:55:49 +0100

    Draft hot/sunny day shutters

--- 0/Automation/Shutters.ts
+++ 0/Automation/Shutters.ts
@@ -5,7 +5,16 @@ const sunset = schedule({ astro: 'sunsetStart' }, async () => {
 
 log(`Next sunrise: ${getAstroDate('sunrise')}`);
 const sunrise = schedule({ astro: 'sunrise' }, async () => {
-  await setStateAsync('scene.0.Shutters.Day', true);
+  const maxTempToday = await getStateAsync(
+    'daswetter.0.NextDays.Location_1.Day_1.Maximale_Temperatur_value',
+  );
+
+  if (maxTempToday.val >= 25) {
+    await setStateAsync('scene.0.Shutters.Sunny_day', true);
+    Notifier.notify('It is going to be sunny today!');
+  } else {
+    await setStateAsync('scene.0.Shutters.Day', true);
+  }
 });
 
 onStop(() => [sunset, sunrise].forEach(scheduled => clearSchedule(scheduled)));
