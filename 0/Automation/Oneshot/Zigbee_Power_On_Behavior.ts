0413aa396df63f6814363b7648523010ca6c05b4
Alexander Groß <agross@therightstuff.de>
2021-03-14 13:25:07 +0100

    Set power-on behavior of Hue lights

---
+++ 0/Automation/Oneshot/Zigbee_Power_On_Behavior.ts
@@ -0,0 +1,34 @@
+$('state[id=zigbee.*.available](functions=Light)').each(id => {
+  const match = id.match(/^(zigbee\.\d+)\.([^.]+)\.available$/);
+
+  const instance = match[1];
+  const device = match[2];
+
+  const name = getObject(`${instance}.${device}`).common.name;
+
+  sendTo(
+    instance,
+    'SendToDevice',
+    {
+      device: device,
+      // https://www.zigbee2mqtt.io/devices/5062431P7.html#power-on-behavior
+      payload: {
+        hue_power_on_behavior: 'recover',
+      },
+    },
+    result => {
+      if (result.success) {
+        log(`Configured ${instance}.${device} (${name})`, 'info');
+      } else {
+        log(
+          `Error configuring ${instance}.${device} (${name}): ${JSON.stringify(
+            result,
+          )}`,
+          'error',
+        );
+      }
+    },
+  );
+});
+
+stopScript(undefined);
