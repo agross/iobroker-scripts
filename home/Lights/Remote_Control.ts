59472a49d1a230d8058fc57e82e5c990c6a0d21e
Alexander Groß <agross@therightstuff.de>
2021-10-21 22:10:28 +0200

    Extract remotes into global such that they can be reused

---
+++ home/Lights/Remote_Control.ts
@@ -0,0 +1,149 @@
+const remotes = [
+  new Remotes.TradfriDimmer({
+    // Kitchen TRADFRI on/off switch
+    device: 'zigbee.1.588e81fffe2bacf4',
+    dim: {
+      brightnessChange: Remotes.BRIGHTNESS_CHANGE,
+      lights: Remotes.DimmableLights.for(
+        ...new Remotes.ObjectsWithStateQuery({
+          rooms: 'kitchen',
+          functions: 'light',
+        }).values(),
+      ),
+    },
+    cycle: {
+      off: 'scene.0.Kitchen.Lights',
+      on: [
+        'scene.0.Kitchen.Lights_Low',
+        'scene.0.Kitchen.Lights_Downlight',
+        'scene.0.Kitchen.Lights_Downlight_+_Dining',
+        'scene.0.Kitchen.Lights_Downlight_+_Kitchen',
+        'scene.0.Kitchen.Lights_Bright',
+        'scene.0.Kitchen.Lights_Cozy',
+      ],
+    },
+  }),
+  new Remotes.TradfriDimmer({
+    // Bedroom TRADFRI on/off switch
+    device: 'zigbee.1.588e81fffe17a8ca',
+    dim: {
+      brightnessChange: Remotes.BRIGHTNESS_CHANGE,
+      lights: Remotes.DimmableLights.for(
+        ...new Remotes.ObjectsWithStateQuery({
+          rooms: 'bedroom',
+          functions: 'light',
+        }).values(),
+      ),
+    },
+    cycle: {
+      off: 'scene.0.Lights.In_Bed',
+      on: [
+        'scene.0.Bedroom.Lights_Cozy',
+        'scene.0.Bedroom.Lights_Low',
+        'scene.0.Bedroom.Lights_Bright',
+      ],
+    },
+  }),
+  new Remotes.Philips({
+    // Hue Remote RWL021
+    device: 'zigbee.1.001788010872fbc4',
+    dim: {
+      brightnessChange: Remotes.BRIGHTNESS_CHANGE,
+      lights: Remotes.DimmableLights.for(
+        ...new Remotes.ObjectsWithStateQuery({
+          rooms: 'living_room',
+          functions: 'light',
+        }).values(),
+      ),
+    },
+    cycle: {
+      off: 'scene.0.Living Room.Lights',
+      on: [
+        'scene.0.Living Room.Lights_Cozy',
+        'scene.0.Living Room.Lights_TV',
+        'scene.0.Living Room.Lights_Reading',
+        'scene.0.Living Room.Lights_Bright',
+      ],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.office.power.stat.shelly1-4',
+    cycle: {
+      off: 'scene.0.Office.Lights',
+      on: [
+        'scene.0.Office.Lights_Cozy',
+        'scene.0.Office.Lights_Low',
+        'scene.0.Office.Lights_Bright',
+      ],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.utility-room.power.stat.shelly1-1',
+    toggle: {
+      states: ['scene.0.Utility Room.Entered'],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.bedroom.power.stat.shelly1-2',
+    cycle: {
+      off: 'scene.0.Bedroom.Lights',
+      on: [
+        'scene.0.Bedroom.Lights_Bright',
+        'scene.0.Bedroom.Lights_Low',
+        'scene.0.Bedroom.Lights_Cozy',
+      ],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.hall.power.stat.shelly1-3',
+    toggle: {
+      off: ['scene.0.Bathroom.Lights'],
+      states: ['scene.0.Bathroom.Lights_Bright'],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.hall.power.stat.shelly1-8',
+    toggle: { states: ['scene.0.Leaving_Home'] },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.hall.power.stat.shelly1-7',
+    toggle: {
+      off: ['scene.0.Kitchen.Lights'],
+      states: ['scene.0.Kitchen.Lights_Bright'],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.kitchen.power.stat.shelly1-9',
+    toggle: {
+      off: ['scene.0.Kitchen.Lights'],
+      states: ['scene.0.Kitchen.Lights_Bright'],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.living-room.power.stat.shelly1-5',
+    toggle: {
+      off: ['scene.0.Living Room.Lights'],
+      states: ['scene.0.Living Room.Lights_Bright'],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.hall.power.stat.shelly1-6',
+    toggle: {
+      off: ['scene.0.Hall.Lights'],
+      states: ['scene.0.Hall.Lights_Bright'],
+    },
+  }),
+  new Remotes.Shelly({
+    device: 'mqtt.0.home.kitchen.power.stat.shelly1-10',
+    toggle: {
+      off: ['scene.0.Living Room.Lights'],
+      states: ['scene.0.Living Room.Lights_Bright'],
+    },
+  }),
+];
+
+const subscriptions = remotes.map(remote => remote.setUp());
+
+onStop(() => {
+  subscriptions.forEach(subscription => subscription.unsubscribe());
+});
