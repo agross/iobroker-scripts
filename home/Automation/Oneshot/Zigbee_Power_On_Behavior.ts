d90b44f43aabe45c42be086d1274930a1e86ebd9
Alexander Groß <agross@therightstuff.de>
2022-05-27 14:27:16 +0200

    Set power-on behavior sequentially to be able to correlate error messages (the callback is useless in this case)

--- home/Automation/Oneshot/Zigbee_Power_On_Behavior.ts
+++ home/Automation/Oneshot/Zigbee_Power_On_Behavior.ts
@@ -1,4 +1,8 @@
-$('state[id=zigbee.*.available](functions=light)').each(id => {
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+const setPowerOnBehavior = [
+  ...$('state[id=zigbee.*.available](functions=light)'),
+].map(id => {
   const match = id.match(/^(zigbee\.\d+)\.([^.]+)\.available$/);
 
   const instance = match[1];
@@ -6,29 +10,38 @@ $('state[id=zigbee.*.available](functions=light)').each(id => {
 
   const name = getObject(`${instance}.${device}`).common.name;
 
-  sendTo(
-    instance,
-    'SendToDevice',
-    {
-      device: device,
-      // https://www.zigbee2mqtt.io/devices/5062431P7.html#power-on-behavior
-      payload: {
-        hue_power_on_behavior: 'recover',
+  return () => {
+    sendTo(
+      instance,
+      'SendToDevice',
+      {
+        device: device,
+        // https://www.zigbee2mqtt.io/devices/5062431P7.html#power-on-behavior
+        payload: {
+          hue_power_on_behavior: 'recover',
+        },
       },
-    },
-    result => {
-      if (result.success) {
-        log(`Configured ${instance}.${device} (${name})`, 'info');
-      } else {
-        log(
-          `Error configuring ${instance}.${device} (${name}): ${JSON.stringify(
-            result,
-          )}`,
-          'error',
-        );
-      }
-    },
-  );
+      result => {
+        if (result.success) {
+          log(`Configured ${instance}.${device} (${name})`, 'info');
+        } else {
+          log(
+            `Error configuring ${instance}.${device} (${name}): ${JSON.stringify(
+              result,
+            )}`,
+            'error',
+          );
+        }
+      },
+    );
+  };
 });
+
+await setPowerOnBehavior.reduce(async (_, set) => {
+  await _;
+  set();
+  log('Waiting...');
+  await delay(2000);
+}, Promise.resolve());
 
 stopScript(undefined);
