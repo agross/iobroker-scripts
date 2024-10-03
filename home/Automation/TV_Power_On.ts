18655d1f699182ceae00a264b35519913939e4b7
Alexander Groß <agross@therightstuff.de>
2024-10-03 09:06:41 +0200

    Add script to wake TV on specific interface

---
+++ home/Automation/TV_Power_On.ts
@@ -0,0 +1,12 @@
+import wol from 'wake_on_lan';
+
+const config = {
+  monitor: 'lgtv.0.states.power',
+  mac: 'lgtv.0.states.mac',
+};
+
+on({ id: config.monitor, val: true, ack: false }, _ => {
+  log(`Got power-on command for TV, sending WOL packet on correct interface`);
+
+  wol.wake(getState(config.mac).val, { address: '172.16.0.255' });
+});
