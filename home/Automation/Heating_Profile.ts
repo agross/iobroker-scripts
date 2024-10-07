b567a14a3d17971f2c426a6b314b1181728f3f86
Alexander Groß <agross@therightstuff.de>
2024-10-07 18:25:17 +0200

    Change heating profiles only during heating period

--- home/Automation/Heating_Profile.ts
+++ home/Automation/Heating_Profile.ts
@@ -1,6 +1,8 @@
-import { tap } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 const config = {
+  heatingPeriod: 'hm-rega.0.2322',
   warmProfileWhen: 'ping.0.iobroker.mobile-phone-sonja',
   profile: {
     normal: 'scene.0.Heating.Normal',
@@ -8,8 +10,17 @@ const config = {
   },
 };
 
-const subscription = new Stream<boolean>(config.warmProfileWhen).stream
+const heatingPeriod = new Stream<boolean>(config.heatingPeriod).stream;
+const warm = new Stream<boolean>(config.warmProfileWhen).stream;
+
+const subscription = combineLatest([warm, heatingPeriod])
   .pipe(
+    map(([warm, heatingPeriod]) => ({
+      warm: warm,
+      heatingPeriod: heatingPeriod,
+    })),
+    filter(x => x.heatingPeriod),
+    map(x => x.warm),
     tap(warm => {
       if (warm) {
         setStateChanged(config.profile.warm, true);
