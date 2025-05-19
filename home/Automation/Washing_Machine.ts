c27287af8f1e4bc567ca1e36b08b58afc9f92613
Alexander Groß <agross@therightstuff.de>
2025-05-19 10:51:55 +0200

    Tune washing machine finish threshold

--- home/Automation/Washing_Machine.ts
+++ home/Automation/Washing_Machine.ts
@@ -10,7 +10,7 @@ import {
 
 const config = {
   workingThreshold: 10,
-  finishedThreshold: 14,
+  finishedThreshold: 18,
   powerMonitor: 'alias.0.mqtt.0.home.bathroom.power.gosund-sp111-3.power',
   powerState: 'alias.0.mqtt.0.home.bathroom.power.gosund-sp111-3.state',
   repowerTimeout:
