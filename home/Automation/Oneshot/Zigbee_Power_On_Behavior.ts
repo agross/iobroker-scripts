49c662da783bf8663435e9ef9ae7e4e7370efdd2
Alexander Groß <agross@therightstuff.de>
2025-07-23 17:36:56 +0200

    Use state to set power on behavior

--- home/Automation/Oneshot/Zigbee_Power_On_Behavior.ts
+++ home/Automation/Oneshot/Zigbee_Power_On_Behavior.ts
@@ -1,49 +1,5 @@
-const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+export {}
 
-const setPowerOnBehavior = [
-  ...$('state[id=zigbee.*.available](functions=light)'),
-].map(id => {
-  const match = id.match(/^(zigbee\.\d+)\.([^.]+)\.available$/);
-
-  const instance = match[1];
-  const device = match[2];
-
-  const name = getObject(`${instance}.${device}`).common.name;
-
-  return () => {
-    sendTo(
-      instance,
-      'SendToDevice',
-      {
-        device: device,
-        // https://www.zigbee2mqtt.io/devices/5062431P7.html#power-on-behavior
-        payload: {
-          hue_power_on_behavior: 'recover',
-        },
-      },
-      result => {
-        if (result.success) {
-          log(`Configured ${instance}.${device} (${name})`, 'info');
-        } else {
-          log(
-            `Error configuring ${instance}.${device} (${name}): ${JSON.stringify(
-              result,
-            )}`,
-            'error',
-          );
-        }
-      },
-    );
-  };
-});
-
-// https://github.com/ioBroker/ioBroker.javascript/issues/694#issuecomment-721675742
-export {};
-await setPowerOnBehavior.reduce(async (_, set) => {
-  await _;
-  set();
-  log('Waiting...');
-  await delay(2000);
-}, Promise.resolve());
+await $('state[id=zigbee.*.power_on_behavior](functions=light)').setStateAsync('previous');
 
 stopScript(undefined);
