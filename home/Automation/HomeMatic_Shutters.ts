0d2fe4b711fa13b074f16ae4522f60d3ffb1247b
Alexander Groß <agross@therightstuff.de>
2026-06-29 19:15:09 +0200

    Support HomeMatic "combined" parameter for level and slats

--- home/Automation/HomeMatic_Shutters.ts
+++ home/Automation/HomeMatic_Shutters.ts
@@ -12,6 +12,40 @@ import {
   withLatestFrom,
 } from 'rxjs/operators';
 
+function getUserDataObjectDefinition(baseId: string): ObjectDefinitionRoot {
+  return [
+    ...$('state[id=*.4.COMBINED_PARAMETER]{ID=COMBINED_PARAMETER}'),
+  ].reduce<ObjectDefinitionRoot>((acc, stateId) => {
+    const device = Device.id(stateId);
+    const name = Device.deviceName(stateId);
+
+    const deviceStates: {
+      [id: string]: iobJS.StateCommon;
+    } = {
+      combined: {
+        role: 'level.blind',
+        type: 'string',
+        name: `${name}: Combined parameter of L=<height>,L2=<slats>`,
+        read: true,
+        write: true,
+      },
+    };
+
+    acc[device] = {
+      type: 'device',
+      native: {},
+      common: { name: Device.deviceName(stateId), role: 'blind' },
+      enumIds: ObjectCreator.getEnumIds(stateId, 'rooms', 'functions'),
+      nested: Object.entries(deviceStates).reduce((acc, [id, common]) => {
+        acc[id] = { type: 'state', native: {}, common: common };
+        return acc;
+      }, {} as ObjectDefinitionRoot),
+    };
+
+    return acc;
+  }, {} as ObjectDefinitionRoot);
+}
+
 function getObjectDefinition(baseId: string): ObjectDefinitionRoot {
   // HomeMatic shutters.
   return [
@@ -23,6 +57,19 @@ function getObjectDefinition(baseId: string): ObjectDefinitionRoot {
     const deviceStates: {
       [id: string]: iobJS.StateCommon;
     } = {
+      combined: {
+        alias: {
+          id: {
+            read: `0_userdata.0.${device}.3.COMBINED_PARAMETER`,
+            write: `${device}.4.COMBINED_PARAMETER`,
+          },
+        },
+        role: 'level.blind',
+        type: 'string',
+        name: `${name}: Combined parameter of L=<height>,L2=<slats>`,
+        read: true,
+        write: true,
+      },
       level: {
         alias: {
           id: { read: `${device}.3.LEVEL`, write: `${device}.4.LEVEL` },
@@ -203,6 +250,11 @@ function getObjectDefinition(baseId: string): ObjectDefinitionRoot {
   }, {} as ObjectDefinitionRoot);
 }
 
+await ObjectCreator.create(
+  getUserDataObjectDefinition('0_userdata.0'),
+  '0_userdata.0',
+);
+
 await ObjectCreator.create(getObjectDefinition('alias.0'), 'alias.0');
 
 // When only slats tilt is set we also need to re-set the current level,
@@ -210,13 +262,21 @@ await ObjectCreator.create(getObjectDefinition('alias.0'), 'alias.0');
 // moving before reapplying the level.
 const subscriptions = [
   ...$('state[id=*.4.LEVEL_2]{CONTROL=BLIND_VIRTUAL_RECEIVER.LEVEL_2}'),
-].map(slatsState => {
+].flatMap(slatsState => {
   const device = slatsState.replace(/\.4\.LEVEL_2$/, '');
   const stableState = `${device}.4.PROCESS`;
   const setShutterLevel = `${device}.4.LEVEL`;
+  const getCombinedLevel = `0_userdata.0.${device}.3.COMBINED_PARAMETER`;
   const getShutterLevel = `${device}.3.LEVEL`;
+  const getSlatsLevel = `${device}.3.LEVEL_2`;
+
+  createState(getCombinedLevel, {
+    name: 'COMBINED_PARAMETER from LEVEL and LEVEL_2',
+    type: 'string',
+  });
 
   const shutter = new Stream<number>(getShutterLevel).stream;
+  const slats = new Stream<number>(getSlatsLevel).stream;
 
   const desiredSlats = new Stream<number>({
     id: slatsState,
@@ -232,6 +292,27 @@ const subscriptions = [
     tap(stable => log(`${device} stable: ${stable}`)),
     share(),
   );
+
+  const setCombined = stable
+    .pipe(
+      filter(x => x === true),
+      withLatestFrom(shutter, slats, (_, shutter, slats) => {
+        return { shutter: shutter, slats: slats };
+      }),
+      tap(x =>
+        setState(
+          getCombinedLevel,
+          `L=${x.shutter},L2=${x.slats}`,
+          true,
+          err => {
+            if (err) {
+              log(`Error setting combined data: ${err}`, 'error');
+            }
+          },
+        ),
+      ),
+    )
+    .subscribe();
 
   const isStable = stable.pipe(
     filter(b => b),
@@ -253,7 +334,10 @@ const subscriptions = [
     filter(slats => Number.isInteger(slats)),
   );
 
-  return merge(allowedSlatChanges, bufferedSlatChangesWhileUnstable)
+  const slatsWithShutterLevel = merge(
+    allowedSlatChanges,
+    bufferedSlatChangesWhileUnstable,
+  )
     .pipe(
       withLatestFrom(shutter, (slats, shutter) => {
         return { slats: slats, shutter: shutter };
@@ -272,6 +356,8 @@ const subscriptions = [
       }),
     )
     .subscribe();
+
+  return [setCombined, slatsWithShutterLevel];
 });
 
 onStop(() => subscriptions.forEach(subscription => subscription.unsubscribe()));
